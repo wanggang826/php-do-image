95eb5984d869a36828db4dd9a13f4ee826c3b928
wgg <wgg@carkusoft.com>
2018-11-27 17:22:42 +0800

    update something

--- Image.php
+++ Image.php
@@ -253,33 +253,21 @@ class Image
 		}
 	}
 
+	//下载
 	public function download($file){
 		if ( file_exists ( $file )) {
-
 			header ( 'Content-Description: File Transfer' );
-
 			header ( 'Content-Type: application/octet-stream' );
-
 			header ( 'Content-Disposition: attachment; filename=' . basename ( $file ));
-
 			header ( 'Content-Transfer-Encoding: binary' );
-
 			header ( 'Expires: 0' );
-
 			header ( 'Cache-Control: must-revalidate' );
-
 			header ( 'Pragma: public' );
-
 			header ( 'Content-Length: ' . filesize ( $file ));
-
 			ob_clean ();
-
 			flush ();
-
 			readfile ( $file );
-
 			exit;
-
 		}
 	}
 
